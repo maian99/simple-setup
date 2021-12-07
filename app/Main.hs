31d5436d95ce3defbab99def63be3d69d861d47e
Nguyễn Hải Bình <binhnh@kikaitech.io>
2021-12-07 21:35:07 +0700

    Initial commit

--- app/Main.hs
+++ app/Main.hs
@@ -13,8 +13,11 @@
 
 module Main where
 
-import Control.Monad (void)
+import Control.Monad (void, unless)
+import Control.Lens
 import qualified Data.Map as M
+import Data.Either.Combinators (rightToMaybe)
+import Data.Void (Void)
 import qualified Ledger
 import qualified Ledger.Ada as Ada
 import qualified Ledger.Constraints as Constraints
@@ -27,52 +30,67 @@ import qualified PlutusTx
 import Wallet.Emulator (knownWallet)
 import Prelude
 
-{-
-Test is the data defines the Test output. In this case, DatumType and RedeemerType of all Test outputs are ().
--}
-data Test
+data CDPDatum 
+  = ManagerDatum 
+    { getUsers       :: [Ledger.PubKeyHash]
+    , getTotalMinted :: Integer } 
+  | UserDatum 
+    { getPubKey :: Ledger.PubKeyHash
+    , getLocked :: Integer
+    , getMinted :: Integer }
 
-instance TScripts.ValidatorTypes Test where
-  type DatumType Test = ()
-  type RedeemerType Test = ()
+data CDP
 
-{-
-validateTest is the validation function of Test script. Each time you consume an output from this script,
-you have to pass this validation. The function will take 3 arguments:
-1. The datum of the output you want to consume, in this case its type is () as non datum.
-2. The redeemer contains optional information that you want to push onchain so that you can
-easily check, in this case we don't add redeemer so let redeemer be ().
-3. The script context includes all information of the transaction.
-In this example the validation is always true, which means that you can always consume an utxo.
--}
-{-# INLINEABLE validateTest #-}
-validateTest :: () -> () -> Ledger.ScriptContext -> Bool
-validateTest _ _ _ = True
+data CDPActions 
+  = Open 
+  | Deposit 
+  | Withdraw 
+  | Mint 
+  | Burn
 
-testInstance :: TScripts.TypedValidator Test
-testInstance =
-  TScripts.mkTypedValidator @Test
-    $$(PlutusTx.compile [||validateTest||])
+PlutusTx.unstableMakeIsData ''CDPActions
+PlutusTx.unstableMakeIsData ''CDPDatum
+
+instance TScripts.ValidatorTypes CDP where
+  type DatumType CDP = CDPDatum
+  type RedeemerType CDP = CDPActions
+
+{-# INLINEABLE mkValidator #-}
+mkValidator :: CDPDatum -> CDPActions -> Ledger.ScriptContext -> Bool
+mkValidator _ a _ = case a of 
+  Open -> True
+  Deposit -> True
+  Withdraw -> True
+  Mint -> True
+  Burn -> True
+ 
+compiledValidator :: TScripts.TypedValidator CDP
+compiledValidator = 
+  TScripts.mkTypedValidator @CDP
+    $$(PlutusTx.compile [||mkValidator||])
     $$(PlutusTx.compile [||wrap||])
   where
-    wrap = TScripts.wrapValidator @() @()
+    wrap = TScripts.wrapValidator @CDPDatum @CDPActions
 
-testValidator :: TScripts.Validator
-testValidator = TScripts.validatorScript testInstance
+validatorScript :: TScripts.Validator
+validatorScript = TScripts.validatorScript compiledValidator
 
-testAddress :: Ledger.Address
-testAddress = Ledger.scriptAddress testValidator
+validatorAddress :: Ledger.Address
+validatorAddress = Ledger.scriptAddress validatorScript
 
-{-
-mkPolicy is the minting policy function of a token. A token type is represented
-by AssetClass, contains 2 fields:
-1. Value.CurrencySymbol: actually this is minting policy of the token, be hashed
-to PlutusTx.BuiltinByteString (ByteString).
-2. Value.TokenName: name of the token.
-Anytime you want to mint a token, you have to pass the minting policy which is
-included in the currency symbol of the token. As the minting policy is always true,
-you can always mint new token.
--}
+type CDPSchema = Contract.Endpoint "Open" ()
+    Contract..\/ Contract.Endpoint "Deposit" Integer
+    Contract..\/ Contract.Endpoint "Withdraw" Integer
+    Contract..\/ Contract.Endpoint "Mint" Integer
+    Contract..\/ Contract.Endpoint "Burn" Integer
+    Contract..\/ Contract.Endpoint "Init" ()
+
+getDatum :: PlutusTx.FromData b => Ledger.ChainIndexTxOut -> Maybe b
+getDatum o = preview Ledger.ciTxOutDatum o >>= rightToMaybe >>= (PlutusTx.fromBuiltinData . Ledger.getDatum)
+
+getValue :: Ledger.ChainIndexTxOut -> Ledger.Value
+getValue = view Ledger.ciTxOutValue
+
 {-# INLINEABLE mkPolicy #-}
 mkPolicy :: () -> Ledger.ScriptContext -> Bool
 mkPolicy _ _ = True
@@ -85,67 +103,205 @@ mintingPolicy =
 mintingPolicyHash :: Scripts.MintingPolicyHash
 mintingPolicyHash = Scripts.mintingPolicyHash mintingPolicy
 
-testCurrencySymbol :: Value.CurrencySymbol
-testCurrencySymbol = Value.mpsSymbol mintingPolicyHash
+myCurrencySymbol :: Value.CurrencySymbol
+myCurrencySymbol = Value.mpsSymbol mintingPolicyHash
 
-testTokenName :: Value.TokenName
-testTokenName = "TEST"
+myTokenName :: Value.TokenName
+myTokenName = "iTSLA"
 
-type TestSchema = Contract.Endpoint "Init" () Contract..\/ Contract.Endpoint "Consume" ()
-
-{-
-This is the initial endpoint to mint 10 TEST tokens and create an output
-at Test script contains all 10 TEST tokens and 100 Ada.
--}
 initOutput :: forall w s. Contract.Contract w s Contract.ContractError ()
 initOutput = do
-  let lookups =
-        Constraints.mintingPolicy mintingPolicy
-          <> Constraints.typedValidatorLookups testInstance
-      val = Value.assetClassValue (Value.assetClass testCurrencySymbol testTokenName) 10
-      tx =
-        Constraints.mustMintValue val
-          <> Constraints.mustPayToTheScript () (Ada.lovelaceValueOf 100 <> val)
+  let lookups = Constraints.typedValidatorLookups compiledValidator
+      tx      = Constraints.mustPayToTheScript (ManagerDatum [] 0) (Ada.lovelaceValueOf 1708)
   void $ Contract.submitTxConstraintsWith lookups tx >>= Contract.awaitTxConfirmed . Ledger.getCardanoTxId
 
-{-
-This endpoint we consume the previous utxo and repay another one contains
-50 Ada, which means that you get 50 Ada and 10 TEST tokens to your wallet.
--}
-consume :: Contract.Contract w s Contract.ContractError ()
-consume = do
-  utxos <- M.toList <$> Contract.utxosAt testAddress
-  let (oref, o) = head utxos
-  let lookups =
-        Constraints.typedValidatorLookups testInstance
-          <> Constraints.unspentOutputs (M.fromList [(oref, o)])
-          <> Constraints.otherScript testValidator
-      constraints =
-        Constraints.mustSpendScriptOutput oref (Scripts.Redeemer (PlutusTx.toBuiltinData @() ()))
-          <> Constraints.mustPayToTheScript () (Ada.lovelaceValueOf 50)
-  Contract.submitTxConstraintsWith lookups constraints >>= Contract.awaitTxConfirmed . Ledger.getCardanoTxId
-
-testEndpoints :: Contract.Contract () TestSchema Contract.ContractError ()
-testEndpoints = Contract.selectList [open, update] >> testEndpoints
+openCDP :: Contract.Contract w s Contract.ContractError ()
+openCDP = do
+  managers <- M.filter containsAuthToken <$> Contract.utxosAt validatorAddress
+  myKey <- Contract.ownPubKeyHash 
+  let (oref, o) = head $ M.toList managers
+      mbUsers = getDatum @CDPDatum o
+  case mbUsers of
+    Nothing                      -> Contract.throwError "List of users is not available"
+    Just (ManagerDatum users tm) -> 
+      if myKey `elem` users then Contract.throwError "This user has already openned a CDP"  
+      else void $ Contract.submitTxConstraintsWith lookups tx >>= Contract.awaitTxConfirmed . Ledger.getCardanoTxId
+           where lookups = Constraints.typedValidatorLookups compiledValidator
+                        <> Constraints.otherScript validatorScript
+                        <> Constraints.unspentOutputs (M.fromList [(oref, o)])
+                 tx      = Constraints.mustSpendScriptOutput oref (Scripts.Redeemer $ PlutusTx.toBuiltinData Open)
+                        <> Constraints.mustPayToTheScript (ManagerDatum (myKey : users) tm) (getValue o)
+                        <> Constraints.mustPayToTheScript (UserDatum myKey 0 0) (Ada.lovelaceValueOf 1012)
   where
-    open :: Contract.Promise () TestSchema Contract.ContractError ()
-    open = Contract.endpoint @"Init" $ \_ -> Contract.handleError Contract.logError initOutput
+    containsAuthToken :: Ledger.ChainIndexTxOut -> Bool 
+    containsAuthToken o = valid $ getDatum @CDPDatum o
+      where valid (Just (ManagerDatum a _)) = True
+            valid _                         = False 
 
-    update :: Contract.Promise () TestSchema Contract.ContractError ()
-    update = Contract.endpoint @"Consume" $ \_ -> Contract.handleError Contract.logError consume
+depositCDP :: Integer -> Contract.Contract w s Contract.ContractError ()
+depositCDP amount = do
+  myKey <- Contract.ownPubKeyHash
+  managers <- M.filter containsAuthToken <$> Contract.utxosAt validatorAddress
+  myOutputs <- M.filter (containsMyKey myKey) <$> Contract.utxosAt validatorAddress
+  let (oref, o) = head $ M.toList managers
+      (uoref, uo) = head $ M.toList myOutputs
+      mbUsers = getDatum @CDPDatum o
+      userDt  = getDatum @CDPDatum uo
+  case mbUsers of
+    Just (ManagerDatum users tm) -> 
+      case userDt of
+        Just (UserDatum _ lk mt) ->
+          if not $ myKey `elem` users then Contract.throwError "This user has not openned a CDP"
+          else void $ Contract.submitTxConstraintsWith lookups tx >>= Contract.awaitTxConfirmed . Ledger.getCardanoTxId
+               where lookups = Constraints.typedValidatorLookups compiledValidator
+                            <> Constraints.otherScript validatorScript
+                            <> Constraints.unspentOutputs (M.fromList [(uoref, uo)])
+                     tx      = Constraints.mustSpendScriptOutput uoref (Scripts.Redeemer $ PlutusTx.toBuiltinData Deposit)
+                            <> Constraints.mustPayToTheScript (UserDatum myKey (lk + amount) mt) (getValue uo <> Ada.lovelaceValueOf amount) 
+        _ -> Contract.throwError "User's datum is not available"
+    _ -> Contract.throwError "List of users is not available"
+  where
+    containsAuthToken :: Ledger.ChainIndexTxOut -> Bool 
+    containsAuthToken o = valid $ getDatum @CDPDatum o
+      where valid (Just (ManagerDatum a _)) = True
+            valid _                         = False
+    containsMyKey :: Ledger.PubKeyHash -> Ledger.ChainIndexTxOut -> Bool
+    containsMyKey mk o = valid $ getDatum @CDPDatum o
+      where valid (Just (UserDatum k _ _)) = k == mk
+            valid _                        = False
 
-{-
-To test your contract, use EmulatorTrace
--}
+withdrawCDP :: Integer -> Contract.Contract w s Contract.ContractError ()
+withdrawCDP amount = do
+  myKey <- Contract.ownPubKeyHash
+  managers <- M.filter containsAuthToken <$> Contract.utxosAt validatorAddress
+  myOutputs <- M.filter (containsMyKey myKey) <$> Contract.utxosAt validatorAddress
+  let (oref, o) = head $ M.toList managers
+      (uoref, uo) = head $ M.toList myOutputs
+      mbUsers = getDatum @CDPDatum o
+      userDt  = getDatum @CDPDatum uo
+  case mbUsers of
+    Just (ManagerDatum users tm) -> 
+      case userDt of
+        Just (UserDatum _ lk mt) ->
+          if not $ myKey `elem` users then Contract.throwError "This user has not openned a CDP"
+          else if ((Ada.fromValue $ getValue uo) < Ada.lovelaceOf amount) then Contract.throwError "The withdrawal amount exceeds the locked value" 
+               else if (1.3 * ((fromIntegral lk) - (fromIntegral amount)) < 1.5 * 709.6 * (fromIntegral mt)) then Contract.throwError "The withdrawal amount breaks the ratio"
+                    else void $ Contract.submitTxConstraintsWith lookups tx >>= Contract.awaitTxConfirmed . Ledger.getCardanoTxId
+                         where lookups = Constraints.typedValidatorLookups compiledValidator
+                                      <> Constraints.otherScript validatorScript
+                                      <> Constraints.unspentOutputs (M.fromList [(uoref, uo)])
+                               tx      = Constraints.mustSpendScriptOutput uoref (Scripts.Redeemer $ PlutusTx.toBuiltinData Withdraw)
+                                      <> Constraints.mustPayToTheScript (UserDatum myKey (lk - amount) mt) (getValue uo <> Ada.lovelaceValueOf (-amount)) 
+        _ -> Contract.throwError "User's datum is not available"
+    _ -> Contract.throwError "List of users is not available"
+  where
+    containsAuthToken :: Ledger.ChainIndexTxOut -> Bool 
+    containsAuthToken o = valid $ getDatum @CDPDatum o
+      where valid (Just (ManagerDatum a _)) = True
+            valid _                         = False
+    containsMyKey :: Ledger.PubKeyHash -> Ledger.ChainIndexTxOut -> Bool
+    containsMyKey mk o = valid $ getDatum @CDPDatum o
+      where valid (Just (UserDatum k _ _)) = k == mk
+            valid _                        = False
+
+mintCDP :: Integer -> Contract.Contract w s Contract.ContractError ()
+mintCDP amount = do
+  myKey <- Contract.ownPubKeyHash
+  managers <- M.filter containsAuthToken <$> Contract.utxosAt validatorAddress
+  myOutputs <- M.filter (containsMyKey myKey) <$> Contract.utxosAt validatorAddress
+  let (oref, o) = head $ M.toList managers
+      (uoref, uo) = head $ M.toList myOutputs
+      mbUsers = getDatum @CDPDatum o
+      userDt  = getDatum @CDPDatum uo
+  case mbUsers of
+    Just (ManagerDatum users tm) -> 
+      case userDt of
+        Just (UserDatum _ lk mt) ->
+          if not $ myKey `elem` users then Contract.throwError "This user has not openned a CDP" 
+          else if (1.3* (fromIntegral lk) < 1.5 * 709.67 * ((fromIntegral mt) + (fromIntegral amount))) then Contract.throwError "The minting amount breaks the ratio"
+               else void $ Contract.submitTxConstraintsWith lookups tx >>= Contract.awaitTxConfirmed . Ledger.getCardanoTxId
+                    where lookups = Constraints.typedValidatorLookups compiledValidator
+                                 <> Constraints.otherScript validatorScript
+                                 <> Constraints.unspentOutputs (M.fromList [(uoref, uo)])
+                                 <> Constraints.mintingPolicy mintingPolicy
+                          val = Value.assetClassValue (Value.assetClass myCurrencySymbol myTokenName) amount
+                          tx      = Constraints.mustMintValue val
+                                 <> Constraints.mustSpendScriptOutput uoref (Scripts.Redeemer $ PlutusTx.toBuiltinData Mint)
+                                 <> Constraints.mustSpendScriptOutput oref (Scripts.Redeemer $ PlutusTx.toBuiltinData Mint)
+                                 <> Constraints.mustPayToTheScript (UserDatum myKey lk (mt + amount)) (getValue uo)
+                                 <> Constraints.mustPayToTheScript (ManagerDatum (myKey : users) (tm + amount)) (getValue o) 
+        _ -> Contract.throwError "User's datum is not available"
+    _ -> Contract.throwError "List of users is not available"
+  where
+    containsAuthToken :: Ledger.ChainIndexTxOut -> Bool 
+    containsAuthToken o = valid $ getDatum @CDPDatum o
+      where valid (Just (ManagerDatum a _)) = True
+            valid _                         = False
+    containsMyKey :: Ledger.PubKeyHash -> Ledger.ChainIndexTxOut -> Bool
+    containsMyKey mk o = valid $ getDatum @CDPDatum o
+      where valid (Just (UserDatum k _ _)) = k == mk
+            valid _                        = False
+
+burnCDP :: Integer -> Contract.Contract w s Contract.ContractError ()
+burnCDP amount = do
+  myKey <- Contract.ownPubKeyHash
+  managers <- M.filter containsAuthToken <$> Contract.utxosAt validatorAddress
+  myOutputs <- M.filter (containsMyKey myKey) <$> Contract.utxosAt validatorAddress
+  let (oref, o) = head $ M.toList managers
+      (uoref, uo) = head $ M.toList myOutputs
+      mbUsers = getDatum @CDPDatum o
+      userDt  = getDatum @CDPDatum uo
+  case mbUsers of
+    Just (ManagerDatum users tm) -> 
+      case userDt of
+        Just (UserDatum _ lk mt) ->
+          if not $ myKey `elem` users then Contract.throwError "This user has not openned a CDP" 
+          else if (amount > tm) then Contract.throwError "The amount exceeds the current maximal burning size"
+               else void $ Contract.submitTxConstraintsWith lookups tx >>= Contract.awaitTxConfirmed . Ledger.getCardanoTxId
+                    where lookups = Constraints.typedValidatorLookups compiledValidator
+                                 <> Constraints.otherScript validatorScript
+                                 <> Constraints.unspentOutputs (M.fromList [(uoref, uo)])
+                                 <> Constraints.mintingPolicy mintingPolicy
+                          val     = Value.assetClassValue (Value.assetClass myCurrencySymbol myTokenName) (-amount)
+                          tx      = Constraints.mustMintValue val
+                                 <> Constraints.mustSpendScriptOutput oref (Scripts.Redeemer $ PlutusTx.toBuiltinData Mint)
+                                 <> Constraints.mustPayToTheScript (ManagerDatum (myKey : users) (tm - amount)) (getValue o) 
+        _ -> Contract.throwError "User's datum is not available"
+    _ -> Contract.throwError "List of users is not available"
+  where
+    containsAuthToken :: Ledger.ChainIndexTxOut -> Bool 
+    containsAuthToken o = valid $ getDatum @CDPDatum o
+      where valid (Just (ManagerDatum a _)) = True
+            valid _                         = False
+    containsMyKey :: Ledger.PubKeyHash -> Ledger.ChainIndexTxOut -> Bool
+    containsMyKey mk o = valid $ getDatum @CDPDatum o
+      where valid (Just (UserDatum k _ _)) = k == mk
+            valid _                        = False
+
+myEndpoints :: Contract.Contract () CDPSchema Contract.ContractError ()
+myEndpoints = Contract.selectList [init, open, deposit, withdraw, mint, burn] >> myEndpoints
+  where 
+    init = Contract.endpoint @"Init" $ \_ -> Contract.handleError Contract.logError $ initOutput  
+    open = Contract.endpoint @"Open" $ \_ -> Contract.handleError Contract.logError openCDP
+    deposit = Contract.endpoint @"Deposit" $ \a -> Contract.handleError Contract.logError $ depositCDP a
+    withdraw = Contract.endpoint @"Withdraw" $ \a -> Contract.handleError Contract.logError $ withdrawCDP a
+    mint = Contract.endpoint @"Mint" $ \a -> Contract.handleError Contract.logError $ mintCDP a
+    burn = Contract.endpoint @"Burn" $ \a -> Contract.handleError Contract.logError $ burnCDP a
+
+
 main :: IO ()
 main = Trace.runEmulatorTraceIO $ do
-  -- activate contract from wallet 1. We got function testEndpoints so that we only need to activate all the endpoints once for each wallet.
-  hdl <- Trace.activateContractWallet (knownWallet 1) testEndpoints
+  hdl <- Trace.activateContractWallet (knownWallet 1) myEndpoints
+  w2  <- Trace.activateContractWallet (knownWallet 2) myEndpoints
 
-  -- Endpoint to init output.
   Trace.callEndpoint @"Init" hdl ()
   void $ Trace.waitNSlots 1
 
-  -- Endpoint to consume. This case using the activation from wallet 1 but you can try to activate from another wallet.
-  Trace.callEndpoint @"Consume" hdl ()
+  Trace.callEndpoint @"Open" hdl ()
+  void $ Trace.waitNSlots 1
+  
+  Trace.callEndpoint @"Deposit" hdl 100000
+  void $ Trace.waitNSlots 1
+  
+  Trace.callEndpoint @"Withdraw" hdl 190000
   void $ Trace.waitNSlots 1
